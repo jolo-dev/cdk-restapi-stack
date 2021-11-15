bb7f66fc47b7dcf8d285edbd46f4e5ac9d9a00b6
John Long <johnyscrazy@gmail.com>
2021-11-15 18:24:18 +0100

    More improvements on the VS Codetour

--- .projenrc.ts
+++ .projenrc.ts
@@ -17,9 +17,6 @@ const project = new AwsCdkTypeScriptApp( {
   deps: ['dotenv'], /* Runtime dependencies of this module. */
   description: 'Infrastructure written in CDK', /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: ['@tsconfig/recommended', 'husky'], /* Build dependencies for this module. */
-  gitignore: ['.env', 'build'],
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
+  gitignore: ['.env', 'build', '.DS_Store'],
 } );
 project.synth();
