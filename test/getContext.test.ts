b24850df8e5706dea2366a27ef3d6db5aa166bb1
John Long <johnyscrazy@gmail.com>
2021-11-26 11:53:55 +0100

    Setting context and add more tests

--- test/getContext.test.ts
+++ test/getContext.test.ts
@@ -8,7 +8,7 @@ const ssmClientMock = mockClient(SSMClient);
 const callServiceCatalogProduct = require('../src/utils/callServiceCatalog');
 
 describe('getNetworkingContext', () => {
-  it.skip('should return the correct context for networking when all parameters in SSM Parameter Store', async () => {
+  it('should return the correct context for networking when all parameters in SSM Parameter Store', async () => {
     ssmClientMock.on(GetParametersCommand).resolves({
       Parameters: [{ Name: '/networking/vpc/id', Value: 'vpcId' },
         { Name: '/networking/private-subnet-1/id', Value: 'privateSubnet1' },
@@ -24,7 +24,7 @@ describe('getNetworkingContext', () => {
     });
   });
 
-  it.only('should start launching product when no parameters in SSM Parameter Store', async () => {
+  it('should start launching product when no parameters in SSM Parameter Store', async () => {
     ssmClientMock.on(GetParametersCommand).resolves({
       InvalidParameters: ['/networking/vpc/id', '/networking/private-subnet-1/id', '/networking/private-subnet-2/id'],
     });
@@ -42,14 +42,41 @@ describe('getNetworkingContext', () => {
     });
   });
 
-  it.skip('should return vpcID for networking because its in SSM Parameter Store but should launch Private Subnets and return their IDs', async () => {
+  it('should return vpcID for networking because its in SSM Parameter Store but should launch Private Subnets and return their IDs', async () => {
+    ssmClientMock.on(GetParametersCommand).resolves({
+      Parameters: [{ Name: '/networking/vpc/id', Value: 'vpcId' }],
+      InvalidParameters: ['/networking/private-subnet-1/id', '/networking/private-subnet-2/id'],
+    });
 
+    jest.spyOn(callServiceCatalogProduct, 'callServiceCatalogProduct')
+      .mockResolvedValueOnce('privateSubnet1')
+      .mockResolvedValueOnce('privateSubnet2');
+
+    const context = await getNetworkingContext();
+    expect(context).toStrictEqual({
+      vpcId: 'vpcId',
+      privateSubnet1: 'privateSubnet1',
+      privateSubnet2: 'privateSubnet2',
+    });
   });
 
-  // it.skip('should return the networking object consist of vpcId, privateSubnet1, privateSubnet2', async () => {
-  //   const params: Parameter[] = [{ Name: 'Foo', Value: 'Bar' }];
-  //   const params2: number[] = [0];
-  //   await getParams(params);
-  //   await getParams(params2);
-  // });
+  it.only('should return vpcId with three subnets', async () => {
+    ssmClientMock.on(GetParametersCommand).resolves({
+      Parameters: [{ Name: '/networking/vpc/id', Value: 'vpcId' }],
+      InvalidParameters: ['/networking/private-subnet-1/id', '/networking/private-subnet-2/id', '/networking/private-subnet-3/id'],
+    });
+
+    jest.spyOn(callServiceCatalogProduct, 'callServiceCatalogProduct')
+      .mockResolvedValueOnce('privateSubnet1')
+      .mockResolvedValueOnce('privateSubnet2')
+      .mockResolvedValueOnce('privateSubnet3');
+
+    const context = await getNetworkingContext();
+    expect(context).toStrictEqual({
+      vpcId: 'vpcId',
+      privateSubnet1: 'privateSubnet1',
+      privateSubnet2: 'privateSubnet2',
+      privateSubnet3: 'privateSubnet3',
+    });
+  });
 });
